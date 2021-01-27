e4e50939ab60841c1beff757870ce7baccd8b923
vikingovich <75033207+vikingovich@users.noreply.github.com>
2021-01-27 23:08:24 +0300

    Delete src/ru/Java2/lesson1 directory

--- src/ru/Java2/lesson1/Inter.java
+++
@@ -1,9 +0,0 @@
-package ru.Java2.lesson1;
-
-public interface Inter {
-
-    public abstract void jumper(Wall height);
-    public abstract void runner(RaceTrack distance);
-
-
-}
--- src/ru/Java2/lesson1/Trials.java
+++
@@ -1,4 +0,0 @@
-package ru.Java2.lesson1;
-
-public interface Trials {
-}
